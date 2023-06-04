7b96999ce44ad6de49bc9310655abf57c8c53089
Antoher0101 <firelessball@yandex.ru>
2023-06-04 14:34:25 +0500

    Обновление зависимостей

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.antoher.oop {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
+    requires org.apache.commons.csv;
 
     opens com.antoher.oop to javafx.fxml;
     exports com.antoher.oop;
